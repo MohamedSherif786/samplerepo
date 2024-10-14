496d4886bf0962e8fbca24cc020d8d8ff8db7925
MohamedSherif786 <cksherif2023@gmail.com>
2024-10-14 20:46:49 +0530

    First Commit

---
+++ Sample.java
@@ -0,0 +1,12 @@
+package samplerepo;
+import java.util.*;
+public class Sample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 Numbers");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1+num2;
+        System.out.println("Sum: "+sum);
+    }
+}
