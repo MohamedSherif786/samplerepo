117449ca3f9c4c78927f4ccf0dd66ecf3b6d22aa
MohamedSherif786 <cksherif2023@gmail.com>
2024-10-14 20:50:58 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -6,7 +6,8 @@ public class Sample{
         System.out.println("Enter 2 Numbers");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        int sum = num1+num2;
+        int num3 = sc.nextInt();
+        int sum = num1+num2+num3;
         System.out.println("Sum: "+sum);
     }
 }
